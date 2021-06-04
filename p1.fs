3c26fe7374b77702704560239386332020eaae7c
Max Thrane Nielsen <maxtnielsen@gmail.com>
2021-06-04 18:46:42 +0200

    Todays work

--- p1.fs
+++ p1.fs
@@ -1,33 +1,34 @@
-module tree_rendering
+module tree_rendering =
 
-type Tree<'a> = | Node of 'a * ('a Tree list);;
-type IntTree = Tree<int>;;
-type StringTree = Tree<string>;;
-type StringIntTree = Tree<string,float>;;
-type Program = int
+    type Tree<'a> = | Node of 'a * ('a Tree list);;
+    type IntTree = Tree<int>;;
+    type StringTree = Tree<string>;;
+    type StringIntTree = Tree<string*float>;;
+    type Program = int;;
 
-let movetree (Node(label, x),subtrees) : 'a Tree) (x : real) = ()
+    let movetree (Node(label,subtrees) : 'a Tree) (x : float) : 'a Tree = Node(label+x,subtrees);;
 
-// 2 Property-based testing
+    // 2 Property-based testing
 
-// 3
-let toPSslow (t:StringIntTree) : string =
-    "Not implemented"
+    // 3
+    let toPSslow (t:StringIntTree) : string =
+        "Not implemented";;
 
-// 4
-let toPSfast (t:StringIntTree) : string =
-    "Not implemented"
+    // 4
+    let toPSfast (t:StringIntTree) : string =
+        "Not implemented";;
 
-// 5
-let treeTofile (s:string) (t:StringTree) =
-    failwith "not implemented"
+    // 5
+    let treeTofile (s:string) (t:StringTree) =
+        failwith "not implemented";;
 
-// 6
-let posTreeTofile (s:string) (t:StringTree) =
-    failwith "not implemented"
+    // 6
+    let posTreeTofile (s:string) (t:StringTree) =
+        failwith "not implemented";;
 
-// 10
-let toGeneralTree (p:Program) : StringTree =
-    Node("Not implemented",[])
+    // 10
+    let toGeneralTree (p:Program) : StringTree =
+        Node("Not implemented",[])
 
-
+    let x = 2+2
+    printfn "%i" x
